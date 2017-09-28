f86b719f9722b6a3a4c156dceedda69ef99acc36
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-28 14:31:25 -0700

    reference update

--- Chapter02.copySampleNICs.ps1
+++ Chapter02.copySampleNICs.ps1
@@ -1,5 +1,5 @@
 ﻿Select-AzureRmSubscription -SubscriptionId 9167af2d-c13e-4d34-9a57-8f37dba6ff31
 New-AzureRmResourceGroup -Name copySampleNICs -Location eastus2euap -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName copySampleNICs -TemplateFile .\Chapter02.copySampleNICs.json -storageAccountName ryjonescopynic -adminUsername ryan -dnsNameforLBIP ryjonescopynic -vmSize Standard_D2_v2_Promo -nicCount 2
+New-AzureRmResourceGroupDeployment -ResourceGroupName copySampleNICs -TemplateUri https://raw.githubusercontent.com/rjmax/IgniteUS2017/master/Chapter02.copySampleNICs.json -storageAccountName ryjonescopynic -adminUsername ryan -dnsNameforLBIP ryjonescopynic -vmSize Standard_D2_v2_Promo -nicCount 2
 
--- Chapter03.if.AvailabilitySet.ps1
+++ Chapter03.if.AvailabilitySet.ps1
@@ -1,4 +1,4 @@
 ﻿Select-AzureRmSubscription -SubscriptionId 9167af2d-c13e-4d34-9a57-8f37dba6ff31
 New-AzureRmResourceGroup -Name ifAvailabilitySet -Location eastus2euap -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName ifAvailabilitySet -TemplateFile .\Chapter03.if.AvailabilitySet.json -adminUsername ryan -dnsLabelPrefix ryjonesifavailabilityset -availabilitySet yes
+New-AzureRmResourceGroupDeployment -ResourceGroupName ifAvailabilitySet -TemplateUri https://raw.githubusercontent.com/rjmax/IgniteUS2017/master/Chapter03.if.AvailabilitySet.json -adminUsername ryan -dnsLabelPrefix ryjonesifavailabilityset -availabilitySet yes
