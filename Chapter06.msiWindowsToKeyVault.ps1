6fdee32be675db07b6573ecba5c1de82379abecb
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-28 15:29:59 -0700

    msi windows

---
+++ Chapter06.msiWindowsToKeyVault.ps1
@@ -0,0 +1,9 @@
+﻿New-AzureRmResourceGroup -Name msiWindows -Location westus -Force
+
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName msiWindows -TemplateUri https://raw.githubusercontent.com/rjmax/IgniteUS2017/master/Chapter06.msiWindowsToKeyvault.json
+
+$deployment
+
+$deployment.Outputs.powerShellCommandToGetKeyVaultSecret.Value
+
+Remove-AzureRmResourceGroup -Name msiWindows
