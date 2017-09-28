f830a45109a3de4811a9719d01866561bf937193
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-28 15:19:39 -0700

    msilinux ps1

---
+++ Chapter05.msiLinuxToArm.ps1
@@ -0,0 +1,9 @@
+﻿New-AzureRmResourceGroup -Name msiLinux -Location WestEurope -Force
+
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName msiLinux -TemplateUri https://raw.githubusercontent.com/rjmax/IgniteUS2017/master/Chapter05.msiLinuxToArm.json
+
+$deployment
+
+$deployment.Outputs.commandToLogin.Value
+
+Remove-AzureRmResourceGroup -Name msiLinux
