a1d1d1f7aa897a4c2defa716ff7742c37a64ee75
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-29 05:49:50 -0700

    eg script

---
+++ Chapter20.EventGrid.ps1
@@ -0,0 +1,6 @@
+﻿New-AzureRmResourceGroup -Name EventGrid -Location westus -Force
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName EventGrid -TemplateFile https://raw.githubusercontent.com/rjmax/IgniteUS2017/master/Chapter20.EventGrid.json
+
+New-AzureRmStorageAccount -ResourceGroupName EventGrid -Location westus -Name ryjonesegtest2 -SkuName Standard_LRS
+
